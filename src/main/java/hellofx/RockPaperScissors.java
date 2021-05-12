ed9efcca5010e23a9ac3f16fa258e074613dd3c6
msylliaasen <msylliaasen@gmail.com>
2021-05-11 21:22:55 -0700

    Working game

--- src/main/java/hellofx/RockPaperScissors.java
+++ src/main/java/hellofx/RockPaperScissors.java
@@ -30,17 +30,62 @@ public class RockPaperScissors {
     void buttonPressed(ActionEvent event) {
     	int choicePlayer = (int) (Math.random() * 3);
     	int choicePC = (int) (Math.random() * 3);
-    	
+ 	
+    	//set pictures based on random number
     	if (choicePlayer == 0) {
-    		playerImage.setImage(new Image("rock.png"));
+    		playerImage.setImage(new Image("hellofx/rock.png"));
     	}
     	if (choicePlayer == 1) {
-    		playerImage.setImage(new Image("paper.jpg"));
+    		playerImage.setImage(new Image("hellofx/paper.jpg"));
     	}
     	if (choicePlayer == 2) {
-    		playerImage.setImage(new Image("scissors.jpeg"));
+    		playerImage.setImage(new Image("hellofx/scissors.jpeg"));
+    	}
+    	if (choicePC == 0) {
+    		computerImage.setImage(new Image("hellofx/rock.png"));
+    	}
+    	if (choicePC == 1) {
+    		computerImage.setImage(new Image("hellofx/paper.jpg"));
+    	}
+    	if (choicePC == 2) {
+    		computerImage.setImage(new Image("hellofx/scissors.jpeg"));
+    	}
+    	   	
+    	//determine winner
+    	if (choicePlayer == choicePC) {
+    		win.setText("DRAW");
     	}
     	
+    	//not a draw so chose
+    	else {
+        	//ROCK Relationships 0
+        	if (choicePlayer == 0 && choicePC == 2) {
+        		win.setText("WIN!!!");
+        	}
+        	
+        	if (choicePlayer == 0 && choicePC == 1) {
+        		win.setText("LOSE!!!");
+        	}
+        	
+        	//PAPER Relationships 1
+        	if (choicePlayer == 1 && choicePC == 0) {
+        		win.setText("WIN!!!");
+        	}
+        	
+        	if (choicePlayer == 1 && choicePC == 2) {
+        		win.setText("LOSE!!!");
+        	}
+        	
+        	//SCISSOR Relationships 2
+        	if (choicePlayer == 2 && choicePC == 1) {
+        		win.setText("WIN!!!");
+        	}
+        	
+        	if (choicePlayer == 2 && choicePC == 0) {
+        		win.setText("LOSE!!!");
+        	}
+    	}
+
     }
 
 }
